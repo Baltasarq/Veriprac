fcb27b70c38798ec1bd0e0b63e002ce65d28dd13
baltasarq <baltasarq@gmail.com>
2025-05-28 14:48:39 +0200

    Bumps version to 0.2

--- Veriprac/src/main/java/com/devbaltasarq/veriprac/core/AppInfo.java
+++ Veriprac/src/main/java/com/devbaltasarq/veriprac/core/AppInfo.java
@@ -6,7 +6,7 @@ package com.devbaltasarq.veriprac.core;
 
 public class AppInfo {
     public static final String NAME = "Veriprac";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     public static final String ABOUT = "Verificador/empaquetador de prácticas.";
     public static final String COMPLETE_NAME = NAME + " v" + VERSION;
 }
