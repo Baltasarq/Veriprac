bf4ac47052dbba2ffc19681195d54b91e3350426
baltasarq <baltasarq@gmail.com>
2025-05-28 12:16:25 +0200

    Adds loging and avoids users choosing home dir

--- Veriprac/src/main/java/com/devbaltasarq/veriprac/ui/MainWindowCore.java
+++ Veriprac/src/main/java/com/devbaltasarq/veriprac/ui/MainWindowCore.java
@@ -8,14 +8,23 @@ import com.devbaltasarq.veriprac.core.AppInfo;
 import com.devbaltasarq.veriprac.core.PractVerifier;
 import com.devbaltasarq.veriprac.core.Util;
 
-import javax.swing.*;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Date;
+import java.util.Map;
+import java.util.EnumMap;
+import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 
 public class MainWindowCore {
@@ -25,6 +34,7 @@ public class MainWindowCore {
 
     public MainWindowCore()
     {
+        this.logger = null;
         this.winUi = new MainWindowUI();
         this.verifier = new PractVerifier();
         this.FIELDS_AND_CHECKS = Map.of(
@@ -53,21 +63,25 @@ public class MainWindowCore {
         ));
 
         this.setListeners();
-        this.USER_HOME = System.getProperty( "user.home" );
+        this.USER_HOME = this.retrieveUserHome();
         this.setPath( this.USER_HOME );
     }
-
-    private String getData(Data data)
+        
+    /** @return the user's home dir from the system. */
+    private String retrieveUserHome()
     {
-        String toret = this.DATA_FIELDS.get( data ).getText().trim();
-
-        if ( data == Data.NIF ) {
-            toret = toret.toUpperCase();
+        String userHome = System.getProperty( "user.home" );
+        
+        if ( userHome == null
+          || userHome.trim().length() == 0 )
+        {
+            userHome = ".";
         }
 
-        return toret;
+        return userHome;
     }
 
+    /** Prepares all listeners, for buttons and menus. */
     private void setListeners()
     {
         this.winUi.getOpQuit().addActionListener(
@@ -119,21 +133,41 @@ public class MainWindowCore {
             }
         });
     }
+    
+    private void log(String msg)
+    {
+        if ( this.logger != null ) {
+            this.logger.info( msg );
+        }
+        
+        return;
+    }
 
+    /** Triggered by File >> Quit. */ 
     private void onQuit()
     {
+        this.log( "Terminated." );
+        LogManager.getLogManager().reset();
         System.exit( 0 );
     }
+    
+    /** Triggered by the File >> Open. */
     private void onOpen()
     {
         final var OPEN_DLG = new JFileChooser( this.verifier.getPath().getAbsolutePath() );
+        final int RESPONSE_OK = JFileChooser.APPROVE_OPTION;
+        
         OPEN_DLG.setDialogTitle( "Open directory" );
         OPEN_DLG.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
-        OPEN_DLG.showOpenDialog( this.winUi );
-        final File DIR = OPEN_DLG.getSelectedFile();
 
-        if ( DIR != null ) {
-            this.setPath( DIR.getAbsolutePath() );
+        if ( OPEN_DLG.showOpenDialog( this.winUi ) == RESPONSE_OK ) {
+            final File DIR = OPEN_DLG.getSelectedFile();
+
+            if ( DIR != null ) {
+                this.setPath( DIR.getAbsolutePath() );
+            }
+            
+            this.prepareLog();
         }
 
         return;
@@ -150,7 +184,25 @@ public class MainWindowCore {
     {
         return this.winUi;
     }
+    
+    /** Gets the data, according to the given identifier.
+      * @param data an enumerator pointing to the real data.
+      * @return the value to retrieve.
+      */
+    private String getData(Data data)
+    {
+        String toret = this.DATA_FIELDS.get( data ).getText().trim();
 
+        if ( data == Data.NIF ) {
+            toret = toret.toUpperCase();
+        }
+
+        return toret;
+    }
+
+    /** Changes the PATH to pack&verify.
+      * @param path the new path to a directory with an exercise.
+      */
     private void setPath(String path)
     {
         this.verifier.setPath( new File( path ) );
@@ -162,43 +214,97 @@ public class MainWindowCore {
 
         this.winUi.getEdPath().setText( path );
     }
+    
+    /** Prepares Veriprac's logging capabilities for this exercise. */
+    private void prepareLog()
+    {
+        final Path LOG_FILE_PATH = Paths.get(
+                                    this.verifier.getPath().getAbsolutePath(),
+                                    "veriprac.log" );
+        
+        // Close the current log
+        if ( this.logger != null ) {
+            LogManager.getLogManager().reset();
+        }
+        
+        // Create the log
+        this.logger = Logger.getLogger( this.getClass().getPackageName() );
+        
+        try {
+            final var HANDLER = new FileHandler( LOG_FILE_PATH.toString() );
+            this.logger.addHandler( HANDLER );
+            
+            HANDLER.setFormatter( new SimpleFormatter() {
+                private static final String format = "%1$tF %1$tT %2$-7s %3$3s %n";
 
+                @Override
+                public synchronized String format(LogRecord lr)
+                {
+                    return String.format( format,
+                                            new Date( lr.getMillis() ),
+                                            lr.getLevel().getName(),
+                                            lr.getMessage()
+                    );
+                }
+            });
+            
+            this.log( "Log created with PATH: " + LOG_FILE_PATH );
+        } catch(IOException exc) {
+            JOptionPane.showMessageDialog(
+                                this.getMainUI(),
+                                AppInfo.NAME,
+                                "ERROR creating log" + exc.getMessage(),
+                                JOptionPane.ERROR_MESSAGE );
+        }
+    }
+
+    /** Checks that fields have valid info, and also the exercise itself. */
     private boolean chk()
     {
         boolean statusSet = false;
-        boolean verified;
+        boolean verified = false;
         boolean fieldsChecked;
 
+        this.log( "Chk started... " );
         this.winUi.setStatus();
 
         // Set all checkboxes
         for(final var PAIR: FIELDS_AND_CHECKS.entrySet()) {
             PAIR.getKey().setState( true );
         }
-
+        
         for(final var PAIR: VERIFY_CHECKS.entrySet()) {
-            PAIR.getValue().setState( true );
+            PAIR.getValue().setState( false );
         }
 
         fieldsChecked = this.chkFields();
-        verified = verifier.verify();
+        
+        if ( fieldsChecked ) {
+            verified = verifier.verify();
 
-        if ( !verified ) {
+            this.log( "Checking verifier errors..." );
             for(final PractVerifier.Id ID: PractVerifier.Id.values()) {
                 if ( !this.verifier.doesVerify( ID ) ) {
                     if ( !statusSet ) {
-                        statusSet = true;
+                        statusSet = false;
                         this.winUi.setStatus( ID.getErrorMsg() );
+                        this.log( "Error: " + ID.getErrorMsg() );
                     }
 
-                    VERIFY_CHECKS.get( ID ).setState( false );
+                    this.VERIFY_CHECKS.get( ID ).setState( false );
+                } else {
+                    this.VERIFY_CHECKS.get( ID ).setState( true );
                 }
             }
         }
 
+        this.log( "Chk finished,"
+                    + " fields: " + fieldsChecked
+                    + " and verified: " + verified );
         return fieldsChecked && verified;
     }
 
+    /** Triggered by the Pack button. This starts the magic. */
     private void onPack()
     {
         if ( this.chk() ) {
@@ -223,11 +329,14 @@ public class MainWindowCore {
         return;
     }
 
+    /** Checks that the data fields have valid data. */
     private boolean chkFields()
     {
         boolean toret = true;
+        
+        this.log( "Checking field contents..." );
 
-        // Reset those checkboxes with fails
+        // Set those checkboxes with fails
         for(final var PAIR: FIELDS_AND_CHECKS.entrySet()) {
             boolean val = !( PAIR.getValue().getText().trim().length() < 4 );
 
@@ -238,45 +347,79 @@ public class MainWindowCore {
             PAIR.getKey().setState( val );
             toret &= val;
         }
+        
+        this.log( "All fields at least with some contents... " + toret );
 
         // Check the type choice
         if ( this.getMainUI().getEdType().getSelectedIndex() < 1 ) {
             this.getMainUI().getChkType().setState( false );
 
             toret = false;
+            this.log( "Exercise type not selected." );
         }
-
+        
+        // Check the PATH field
+        final String PATH = this.getMainUI().getEdPath().getText().trim();
+        if ( PATH.equals( this.USER_HOME ) ) {
+            final String PATH_MSG = "Invalid PATH: can't be user's home.";
+            
+            this.getMainUI().getChkPath().setState( false );            
+            toret = false;
+            this.log( PATH_MSG );
+            this.winUi.setStatus( PATH_MSG );
+        }
+        
+        this.log( "Checking fields returns " + toret );
         return toret;
     }
 
+    /** Creates a text file with the data entered. */
     private void createMarksFile() throws IOException
     {
+        final String PATH = this.getData( Data.PATH );
         final String NIF = this.getData( Data.NIF );
         final String SURNAME = this.getData( Data.SURNAME );
         final String NAME = this.getData( Data.NAME );
         final String SUMMARY = this.getData( Data.SUMMARY );
-        final String FILE_NAME = Paths.get(
-                                    this.getData( Data.PATH ),
-                                    "notas.txt" ).toString();
+        final String FILE_NAME = Paths.get( PATH, "notas.txt" ).toString();
+        
+        this.log( "Creating marks file with..." );
+        this.log( "Surname: " + SURNAME );
+        this.log( "Name: " + NAME );
+        this.log( "NIF: " + NIF );
+        this.log( "Home: " + this.USER_HOME );
+        this.log( "Path: " + PATH );
+        this.log( "File: " + FILE_NAME );
 
         try (var output = new PrintStream( FILE_NAME )) {
-            output.println( Util.buildMarksFileContents( NIF, SURNAME, NAME, SUMMARY) );
+            output.println( Util.buildMarksFileContents(
+                                    NIF, SURNAME, NAME, SUMMARY ) );
         }
 
-        return;
+        this.log( "Marks file created." );
     }
 
+    /** Creates the zip file. */
     private void buildZip() throws IOException
     {
         final String SURNAME = this.getData( Data.SURNAME );
         final String NAME = this.getData( Data.NAME );
         final String NIF = this.getData( Data.NIF );
         final String TARGET_PATH = this.getData( Data.PATH );
+        
+        this.log( "Creating ZIP with..." );
+        this.log( "Surname: " + SURNAME );
+        this.log( "Name: " + NAME );
+        this.log( "NIF: " + NIF );
+        this.log( "Home: " + this.USER_HOME );
+        this.log( "Path: " + TARGET_PATH );
 
         Util.buildZip( NIF, SURNAME, NAME, this.USER_HOME, TARGET_PATH );
+        this.log( "ZIP built." );
     }
 
     private final String USER_HOME;
+    private Logger logger;
     private final MainWindowUI winUi;
     private final PractVerifier verifier;
     private final EnumMap<PractVerifier.Id, java.awt.Checkbox> VERIFY_CHECKS;
